3c0f478c3419b2c0406352c6e3f6f34b4314a743
shiftleino <>
2021-11-18 15:00:45 +0200

    Started the project work

--- src/index.py
+++ src/index.py
@@ -1,5 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui.mainwindow import MainWindow
+
 def main():
-    print("Hello World!")
+    app = QApplication(sys.argv)
+    gui = MainWindow()
+    gui.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
