f36ab8e869e8327d0b95a3cbaa96da7e11c9dd2d
shiftleino <>
2021-12-11 13:10:04 +0200

    Added some tests

--- src/tests/services/stock_service_test.py
+++ src/tests/services/stock_service_test.py
@@ -1,6 +1,16 @@
 import unittest
-
+from services.stock_service import StockService
+from database.init_db import initialize_db
+from database.db_connection import get_connection
 
 class TestStockService(unittest.TestCase):
     def setUp(self):
-        pass
+        self.conn = get_connection()
+        initialize_db()
+        self.user_id = 1
+        self.service = StockService(self.user_id, self.conn)
+
+    def test_return_data_empty(self):
+        result = self.service.return_user_data()
+        self.assertEqual(len(result), 0)
+        
